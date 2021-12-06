ed033d34852ccb2b0aa2344020fdbd89cf7cc388
cosund <cornelia.sundqvist@live.se>
2021-12-06 21:42:10 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-function RenderTest(){ console.log("Vue sub-component render test"); return false; }
-function App(props){
-   return  (
-        <div >
-            <p>working</p>
-         </div>
-    );
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <p>Test</p>
+  );
 }
 
-function defaultRoute(){
-  const knownRoutes = ["#search", "#myDrink", "#details"]
-    if(!knownRoutes.includes(window.location.hash)) {
-      window.location.hash="#search";
-  }
-}
-defaultRoute(); // when the application loads, set the default route!
-
-window.addEventListener("hashchange", defaultRoute);
+export default App;

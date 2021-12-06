1c595fcafb86ca9a8ddb74deb6f35549e13ebbfc
cosund <cornelia.sundqvist@live.se>
2021-12-06 21:31:55 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 function RenderTest(){ console.log("Vue sub-component render test"); return false; }
 function App(props){
    return  (
-        <div className="flexparent">
-          <div className="menubar"><MenuView categoryChosen={category => console.log(category)}/></div>
-          <div className="mainContent"><ShowPresenter hash="#filter">
-                                   <FilterView filterResults={props.model}/>
-                                   </ShowPresenter>
-
-                                   <ShowPresenter hash="#details">
-                                   <DetailsView model={props.model}/>
-                                   </ShowPresenter>
-
-                                   <ShowPresenter hash="myDrink">
-                                   <MyDrinkView model={props.model}/>
-                                   </ShowPresenter>
-          </div>
+        <div >
+            <p>working</p>
          </div>
     );
 }

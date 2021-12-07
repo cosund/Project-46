cc6d440d4c621fc99405e03e511d5437c7322a2e
cosund <cornelia.sundqvist@live.se>
2021-12-07 14:18:32 +0100

    style

--- src/App.js
+++ src/App.js
@@ -8,21 +8,28 @@ import './js/drinkModel.js';
 import './js/apiConfig.js';
 import './js/view/style.css';
 
-function App(props) {
-  return (
-    <div className="flexparent">
-    <div className="menubar"><MenuView categoryChosen={category => console.log(category)}/></div>
-    <div className="mainContent">
+function App(props){
+   return  (
+        <div className="flexparent">
+          <div className="menubar"><MenuView categoryChosen={category => console.log(category)}/></div>
+          <div className="mainContent"><ShowPresenter hash="#filter">
+                                   <FilterView filterResults={props.model}/>
+                                   </ShowPresenter>
 
-    <ShowPresenter hash="#filter">
-    <FilterView model={props.model} />
-    </ShowPresenter>
-    </div>
-  );
+                                   <ShowPresenter hash="#details">
+                                   <DetailsView model={props.model}/>
+                                   </ShowPresenter>
+
+                                   <ShowPresenter hash="myDrink">
+                                   <MyDrinkView model={props.model}/>
+                                   </ShowPresenter>
+          </div>
+         </div>
+    );
 }
 
 function defaultRoute(){
-  let knownRoutes = ["#search", "#myDrink", "#details"];
+  const knownRoutes = ["#search", "#myDrink", "#details"]
     if(!knownRoutes.includes(window.location.hash)) {
       window.location.hash="#search";
   }

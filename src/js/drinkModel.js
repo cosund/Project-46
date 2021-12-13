cd76ade641d085f381e0a07445bfbe09d7efb39d
cosund <43178462+cosund@users.noreply.github.com>
2021-12-13 11:42:52 +0100

    Update drinkModel.js

--- src/js/drinkModel.js
+++ src/js/drinkModel.js
@@ -1,9 +1,38 @@
 class DrinkModel{
   constructor(drinks=[], currentDrink=null){
-      //this.observers = [];
+      this.observers = [];
       this.drinks = drinks;
       this.currentDish = currentDrink;
     }
+    addObserver(callback){
+      this.observers = [...this.observers, callback];
+    }
+    removeObserver(callback){
+      this.observers = this.observers.filter((e) => e !== callback);
+    }
+    notifyObservers(){
+      this.observers.forEach(args => {
+        setTimeout(() =>  {
+          try {
+            args();
+          } catch(error){
+            console.log(error);
+          }
+        }, 0)
+      })
+    }
+    addToMenu(drink){
+      const found = this.drinks.filter((e) => e === dish)
+      if(found !== dish){
+        this.drinks = [...this.drinks, drink]
+        this.notifyObservers();
+    }
+    };
+    removeFromMenu(drinkData){
+      const found = this.drinks.filter(e => drinkData === e)
+      if(found[0] === drinkData){
+      this.drinks = this.drinks.filter(drinkID => drinkID.id !== drinkData.id)
+      this.notifyObservers()
+    }
+    }
 }
-
-export default DrinkModel;

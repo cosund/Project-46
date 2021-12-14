af44673491779d24cc670f7a7db435774c79bd03
joarcool <jorre98@gmail.com>
2021-12-14 13:32:22 +0100

    Update Filter

--- src/js/reactjs/filterPresenter.js
+++ src/js/reactjs/filterPresenter.js
@@ -12,22 +12,28 @@ function FilterPresenter(props){
   
     //change value promise 
     React.useEffect( function(){
-          setPromise(DrinkSource.filterIngredient(""));
+          setPromise(DrinkSource.filterIngredient("vodka"));
         }, []); // [] cleanup after an effect has been established
     const [data, error] = usePromise(promise);
   
     //React.Framnet used to return multiple elements instead of div
     return(
       <React.Fragment>
-       <MenuView className="menubar" options={["vodka","tequila", "rum", "gin", "triple sec"]}
-       categoryChosen={category => {
-          //setFilterQuery(category);  //For some reasen bugs out the below
-          setPromise(DrinkSource.filterIngredient(category));
-       }}
-       />
-       {promiseNoData(promise, data, error) ||
-           <FilterView filterResults={data} />}
+        <div className="filterView">
+          <FilterOptions options={["vodka","tequila", "rum", "gin", "triple sec"]} 
+                        categoryChosen={category => {
+                        //setFilterQuery(category);  //For some reasen bugs out the below
+                        setPromise(DrinkSource.filterIngredient(category));
+                        }}
+                        characterChosen={character => {
+                          setPromise(DrinkSource.filterChar(character));
+                        }}
+          />
+          {promiseNoData(promise, data, error) ||
+          <FilterResults filterResults={data} />}
+        </div>
       </React.Fragment>
+  
     )
   }
 
--- src/js/view/filterView.js
+++ src/js/view/filterView.js
@@ -1,9 +1,50 @@
 import './style.css';
 import './drinkSource.js';
 
-function FilterView(props){
+let alphabet = ['A', 'B', 'C', 'D', 'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','V','W','Y','Z'];
+let mainIng = new Array(DrinkSource.listCategories("i").then(data => data));
+function FilterOptions(props){
     return(
-        <div className="filterView">
+        <div className="filterOptions">
+            <div className="ingridientSort">
+                <p className="filterTopic">Sort by Main Ingridient:</p>
+                <p onClick={() => props.categoryChosen("vodka")}>- Vodka</p>
+                <p onClick={() => props.categoryChosen("gin")}>- Gin</p>
+                <p onClick={() => props.categoryChosen("rum")}>- Rum</p>
+                <p onClick={() => props.categoryChosen("tequila")}>- Tequila</p>
+                <p onClick={() => props.categoryChosen("triple sec")}>- Triple sec</p>
+            </div>
+            <div className="colorSort">
+                <p className="filterTopic">Sort by Color:</p>
+                <p onClick={() => props.categoryChosen("gin")}>- Blue</p>
+                <p onClick={() => props.categoryChosen("rum")}>- Red</p>
+                <p onClick={() => props.categoryChosen("tequila")}>- Green</p>
+                <p onClick={() => props.categoryChosen("triple sec")}>- Yellow</p>
+            </div>
+            <div className="categorySort">
+                <p className="filterTopic">Sort by Category:</p>
+                <p onClick={() => props.categoryChosen("gin")}>- Blue</p>
+                <p onClick={() => props.categoryChosen("rum")}>- Red</p>
+                <p onClick={() => props.categoryChosen("tequila")}>- Green</p>
+                <p onClick={() => props.categoryChosen("triple sec")}>- Yellow</p>
+            </div>
+            <div className="abcSort">
+                <p className="filterTopic">Sort by First Letter:</p>
+                {alphabet.map(
+                    function(opt) {
+                        return(
+                            <p key={opt} onClick={() => props.characterChosen(opt)}>- {opt}</p>
+                        )
+                    }
+                )}
+            </div>
+        </div>
+    )
+}
+
+function FilterResults(props){
+    return(
+        <div className="filterResults">
             {props.filterResults.drinks.map(
                 function(opt){
                     return(

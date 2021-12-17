7a3d26fc919d19206fd02d47c9221bd6f8c4e948
cosund <43178462+cosund@users.noreply.github.com>
2021-12-17 15:04:12 +0100

    Startview finished

--- src/js/view/startView.js
+++ src/js/view/startView.js
@@ -3,8 +3,14 @@ import './style.css';
 function StartView(props){
     return(
         <div className="start">
-            <h1>Welcome to this cocktail page</h1>
-            <button onClick={() => window.location.hash="#search"}>Get started</button>
+            <h1 className="rubrik">Welcome to this cocktail page</h1>
+            <div>
+                <p className="text">This webpage was made by Joar&#128511;, Adam&#128511;, Alva&#128511; and Cornelia&#128511;.
+                Explore the world of cocktails and save your favorites you find along the way. </p>
+            </div>
+            <div className="viewButton">
+            <button className="startButton" onClick={() => window.location.hash="#search"}>Get started</button>
+            </div>
         </div>
     );
 }

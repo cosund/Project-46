ab3909e36fb045abe6282d5f88ff93fc48b9595b
joarcool <jorre98@gmail.com>
2021-12-19 23:52:18 +0100

    observer for drinks

--- src/js/reactjs/detailsPresenter.js
+++ src/js/reactjs/detailsPresenter.js
@@ -9,14 +9,15 @@ function DetailsPresenter(props){
   const currentDrink = useModelProperty(props.model, "currentDrink");
   const currentDrinkDetails = useModelProperty(props.model, "currentDrinkDetails");
   const currentDrinkError = useModelProperty(props.model, "currentDrinkError");
+  const drinks = useModelProperty(props.model, "drinks");
   
   return(
       <React.Fragment>
           {promiseNoData(currentDrink, currentDrinkDetails, currentDrinkError) || 
           <DetailsView 
-          isDrinkInMenu = {props.model.drinks.find(d => d.drinks[0].idDrink === currentDrink)}
+          isDrinkInMenu = {drinks.find(d => d.drinks[0].idDrink === currentDrink)}
           drinkDetails = {currentDrinkDetails}
-          drinkAdded = {drinks => myModel.addToMenu(drinks)}
+          drinkAdded = {drink => myModel.addToMenu(drink)}
           />}
       </React.Fragment>
   )
